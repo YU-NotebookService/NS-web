d7fdc201e066a79103293685af2514a356a979eb
dev-joohee <dev.joohee0916@gmail.com>
2024-09-09 01:00:09 +0900

    Design: 헤더 레이아웃 디자인

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import GlobalStyles from './GlobalStyles';
+import Layout from './layouts/header/Layout';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div className="Content">
-        <div>Content</div>
-      </div>
+      <Layout />
     </>
   );
 }
