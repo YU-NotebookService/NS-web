47ac4d31905e437f8b67df628b7c8b4dca836984
dev-joohee <dev.joohee0916@gmail.com>
2024-09-09 00:51:31 +0900

    style: 코드 포맷팅

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import GlobalStyles from './GlobalStyles';
-import Layout from './layouts/header/Layout';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Layout />
       <div className="Content">
         <div>Content</div>
       </div>
