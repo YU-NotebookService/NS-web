ef5e7a9568da65197b2d33fdc500a44fb35c2329
groundinsider <groundinsider24@gmail.com>
2024-09-09 21:45:31 +0900

    Design: 로그인 레이아웃 디자인

--- src/components/LoginLayout-styled.js
+++ src/components/LoginLayout-styled.js
@@ -7,7 +7,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: center; 
 `;
 
 export const LoginBox = styled.div`
