7135cc2ee9009f9116bf900caa5d2823af4c4ea6
groundinsider <groundinsider24@gmail.com>
2024-09-10 21:19:38 +0900

    style: 코드 포맷팅

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import GlobalStyles from './GlobalStyles';
-import Login from './layouts/login/Layout';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Login />
       <div className="Content">
-        
+        <div>Content</div>
       </div>
     </>
   );
