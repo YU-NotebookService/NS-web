31f5ce0a7b04cb775d89279fd3b4137d69c55437
groundinsider <groundinsider24@gmail.com>
2024-09-11 20:47:51 +0900

    Design: 회원가입 레이아웃 디자인

--- src/components/RegisterLayout-styled.js
+++ src/components/RegisterLayout-styled.js
@@ -1,8 +1,18 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  
+  width: 100%;
+  min-width: 850px;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center; 
+`;
 
+export const Title = styled.div`
+  font-size: 30px;
+  
 
 
 
--- src/layouts/register/Layout.js
+++ src/layouts/register/Layout.js
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import {
   Container,
-  
+  Title,
 } from '../../components/RegisterLayout-styled';
+import Button from '../../components/common/Button';
+import Input from '../../components/common/Input';
 
 function Register() {
   return (  
     <Container>
-
+      <Title>회원가입</Title>
 
 
     </Container>
