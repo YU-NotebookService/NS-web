fe2e006261a7ce49c91acb5fc0c4698fb52fff7f
groundinsider <groundinsider24@gmail.com>
2024-09-11 21:21:22 +0900

    Design: 회원가입 레이아웃 디자인

--- src/components/RegisterLayout-styled.js
+++ src/components/RegisterLayout-styled.js
@@ -5,15 +5,51 @@ export const Container = styled.div`
   min-width: 850px;
   background-color: #ffffff;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: center; 
 `;
 
 export const Title = styled.div`
+  margin-top: 30px;
   font-size: 30px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+export const Text = styled.span`
+  width: 100px;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  padding-right: 20px;
+  color: var(--gray-color);
+`;
+
+export const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   
 
 
+`;
 
+export const FormItem = styled.div`
+   width: 500px;
+   margin-top: 30px;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+
+`;
+
+export const NumText = styled.span`
+  width: 10px;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  margin: 0 12px;
+  color: var(--gray-color);
 `;
--- src/layouts/register/Layout.js
+++ src/layouts/register/Layout.js
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import {
   Container,
   Title,
+  Text,
+  Form,
+  FormItem,
+  NumText
 } from '../../components/RegisterLayout-styled';
 import Button from '../../components/common/Button';
 import Input from '../../components/common/Input';
@@ -10,8 +14,68 @@ function Register() {
   return (  
     <Container>
       <Title>회원가입</Title>
+      <Form>
+        <FormItem>
+          <Text>학번</Text>
+          <Input
+          style = {{
+            width: "270px"
+          }}/>
+        </FormItem>
+        <FormItem>
+          <Text>이름</Text>
+          <Input
+            style = {{
+              width: "270px"
+            }}
+          />
+        </FormItem>
+        <FormItem>
+            <Text>새 비밀번호</Text>
+            <Input
+              style = {{
+                width: "270px"
+              }}
+            />
+        </FormItem>
+        <FormItem>
+          <Text>휴대폰 번호</Text>
+          <Input
+            style = {{
+              width: "35x"
+            }}
+          />
+          <NumText>-</NumText>
+          <Input
+            style = {{
+              width: "35px"
+            }}
+          />
+          <NumText>-</NumText>
+          <Input
+            style = {{
+              width: "35px"
+            }}
+          />
+        </FormItem>
+        <FormItem>
+            <Text>이메일</Text>
+            <Input
+              style = {{
+                width: "270px"
+              }}
+            />
+        </FormItem>
+        <Button
+          style = {{
+            width: "450px",
+            marginTop: "30px"
+          }}
+        >
+          가입하기
+        </Button>
 
-
+      </Form>
     </Container>
 
   );
