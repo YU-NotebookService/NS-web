ce1f5cf20a63698f565c94f082f39d8dab9e99a0
groundinsider <groundinsider24@gmail.com>
2024-09-14 18:32:01 +0900

    Design: 회원가입 레이아웃 디자인

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyles from './GlobalStyles';
-import Register from './layouts/register/RegisterLayout';
+import Register from './pages/register/Register';
 
 function App() {
   return (
