9d05a05a59d88dcdea3c93361d2f1ba066f825d1
gugitgugit <rnwnsgur28@gmail.com>
2024-09-20 12:47:05 +0900

    feat: 노트북 목록 조회

---
+++ src/components/common/SearchBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Wrapper } from '../../styles/common/SearchBox-styled';
+import Input from './Input';
+import Button from './Button';
+import { ReactComponent as SearchIcon } from '../../assets/common/searchIcon.svg';
+
+const SearchBox = () => {
+  return (
+    <Wrapper>
+      <Input placeholder="검색어를 입력해주세요." style={{ border: 'none' }} />
+      <Button type="submit" style={{ background: 'none', padding: '0px 10px' }}>
+        <SearchIcon />
+      </Button>
+    </Wrapper>
+  );
+};
+
+export default SearchBox;
