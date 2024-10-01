7206dc75b5fc3e27a272869fccb8ed8b6a66ede8
gugitgugit <rnwnsgur28@gmail.com>
2024-10-01 17:06:05 +0900

    feat: 신규 등록 페이지 제작

--- src/components/common/Register.js
+++ src/components/common/Register.js
@@ -1,38 +1,115 @@
 import React from 'react';
 import {
+  ContentInput,
+  ContentInputTitle,
+  ContentInputWrapper,
+  Count,
+  Error,
+  ErrorWrapper,
+  FormWrapper,
   InputTitle,
   InputWrapper,
+  PicBtn,
+  PicTitle,
+  PicWrapper,
+  PostInput,
+  RegBtn,
   RegisterWrapper,
 } from 'styles/common/Register-styled';
-import Button from './Button';
-import Input from './Input';
+import { useForm } from 'react-hook-form';
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const title = watch('title', '');
+  const os = watch('os', '');
+  const content = watch('content', '');
+
+  const onSubmit = (data) => {
+    console.log('제출된 데이터: ', data);
+  };
+
   return (
-    <RegisterWrapper>
-      <InputWrapper>
-        <InputTitle>
-          제목<span style={{ color: 'var(--gray-color)' }}>(30자 이하)</span>
-        </InputTitle>
-        <Input placeholder="제목을 입력해 주세요." style={{ width: '100%' }} />
-      </InputWrapper>
-      <InputWrapper>
-        <InputTitle>
-          OS<span style={{ color: 'var(--gray-color)' }}>(30자 이하)</span>
-        </InputTitle>
-        <Input placeholder="OS를 입력해 주세요." style={{ width: '100%' }} />
-      </InputWrapper>
-      <InputWrapper>
-        <InputTitle>
-          내용<span style={{ color: 'var(--gray-color)' }}>(500자 이하)</span>
-        </InputTitle>
-        <Input placeholder="내용을 입력해 주세요." style={{ width: '100%' }} />
-      </InputWrapper>
-      <InputWrapper>
-        <InputTitle>사진 첨부</InputTitle>
-        <Input placeholder="사진을 선택해주세요." style={{ width: '100%' }} />
-      </InputWrapper>
-      <Button style={{ width: '200px', margin: 'auto' }}>등록하기</Button>
+    <RegisterWrapper onSubmit={handleSubmit(onSubmit)}>
+      <FormWrapper>
+        <InputWrapper>
+          <InputTitle>
+            제목<span style={{ color: 'var(--gray-color)' }}>(30자 이하)</span>
+          </InputTitle>
+          <ErrorWrapper>
+            <Count isError={!!errors.title}>{title.length}/30</Count>
+            <PostInput
+              placeholder="제목을 입력해 주세요."
+              isError={!!errors.title}
+              {...register('title', {
+                required: '제목은 필수 항목입니다.',
+                maxLength: {
+                  value: 30,
+                  message: '제목은 30자 이하로 입력해주세요.',
+                },
+              })}
+            />
+            {errors.title && <Error>{errors.title.message}</Error>}
+          </ErrorWrapper>
+        </InputWrapper>
+
+        {/* 현재 URL 에 'notebook'이 포함될 시에만 출력 */}
+        {window.location.pathname.includes('notebook') && (
+          <InputWrapper>
+            <InputTitle>
+              OS<span style={{ color: 'var(--gray-color)' }}>(30자 이하)</span>
+            </InputTitle>
+            <ErrorWrapper>
+              <Count isError={!!errors.os}>{os.length}/30</Count>
+              <PostInput
+                placeholder="운영체제를 입력해 주세요."
+                isError={!!errors.os}
+                {...register('os', {
+                  required: '운영체제는 필수 항목입니다.',
+                  maxLength: {
+                    value: 30,
+                    message: '운영체제는 30자 이하로 입력해주세요.',
+                  },
+                })}
+              />
+              {errors.os && <Error>{errors.os.message}</Error>}
+            </ErrorWrapper>
+          </InputWrapper>
+        )}
+        <ContentInputWrapper>
+          <ContentInputTitle>
+            내용<span style={{ color: 'var(--gray-color)' }}>(500자 이하)</span>
+          </ContentInputTitle>
+          <ErrorWrapper>
+            <Count isError={!!errors.content}>{content.length}/500</Count>
+            <ContentInput
+              placeholder="내용을 입력해 주세요."
+              isError={!!errors.content}
+              {...register('content', {
+                required: '내용은 필수 항목입니다.',
+                maxLength: {
+                  value: 500,
+                  message: '내용은 500자 이하로 입력해주세요.',
+                },
+              })}
+            />
+            {errors.content && <Error>{errors.content.message}</Error>}
+          </ErrorWrapper>
+        </ContentInputWrapper>
+        <InputWrapper>
+          <PicTitle>사진 첨부</PicTitle>
+          <PicWrapper>
+            <PostInput placeholder="사진을 선택해주세요." />
+            <PicBtn>찾아보기</PicBtn>
+          </PicWrapper>
+        </InputWrapper>
+      </FormWrapper>
+      <RegBtn>등록하기</RegBtn>
     </RegisterWrapper>
   );
 };
--- src/styles/common/Register-styled.js
+++ src/styles/common/Register-styled.js
@@ -1,16 +1,23 @@
 import styled from 'styled-components';
+import Input from 'components/common/Input';
+import Button from 'components/common/Button';
 
 export const RegisterWrapper = styled.form`
-  padding: 60px 360px 60px 360px;
   border-top: 2px solid var(--main-color);
+`;
+
+export const FormWrapper = styled.div`
+  padding: 30px 0px 60px 250px;
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  border-bottom: 1px solid #dbdbdb;
+  margin-bottom: 50px;
 `;
 
 export const InputWrapper = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-start;
+  margin-top: 20px;
 `;
 
 export const InputTitle = styled.div`
@@ -20,5 +27,100 @@ export const InputTitle = styled.div`
   font-weight: 500;
   line-height: 23px; /* 143.75% */
   letter-spacing: 1.6px;
-  width: 30%;
+  width: 200px;
+  padding-top: 32px;
+`;
+
+export const PostInput = styled(Input)`
+  width: 500px;
+  border: ${(props) =>
+    props.isError ? '1px solid red' : '1px solid #dbdbdb;'};
+`;
+
+export const ContentInputWrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  margin-top: 20px;
+`;
+
+export const ContentInputTitle = styled.div`
+  white-space: nowrap;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 23px; /* 143.75% */
+  letter-spacing: 1.6px;
+  width: 200px;
+  padding-top: 30px;
+`;
+
+export const ContentInput = styled.textarea`
+  padding: 24px 24px;
+  font-family: pretendard;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  border-radius: 10px;
+  border: 1px solid #dbdbdb;
+  width: 500px;
+  height: 500px;
+  &::placeholder {
+    color: #dbdbdb;
+  }
+  border: ${(props) =>
+    props.isError ? '1px solid red' : '1px solid #dbdbdb;'};
+`;
+
+export const PicBtn = styled(Button)`
+  width: 120px;
+  font-size: 15px;
+  margin-left: 20px;
+  position: absolute;
+  right: -140px;
+`;
+
+export const RegBtn = styled(Button)`
+  width: 200px;
+  margin: auto;
+`;
+
+export const Error = styled.div`
+  padding: 5px 0px 0px 24px;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  color: red;
+`;
+
+export const ErrorWrapper = styled.div`
+  width: 550px;
+`;
+
+export const PicWrapper = styled.div`
+  width: 550px;
+  display: flex;
+  position: relative;
+`;
+
+export const Count = styled.div`
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 23px; /* 143.75% */
+  letter-spacing: 1.6px;
+  width: 100%;
+  text-align: end;
+  color: ${(props) => (props.isError ? 'red' : 'var(--gray-color)')};
+`;
+
+export const PicTitle = styled.div`
+  white-space: nowrap;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 23px; /* 143.75% */
+  letter-spacing: 1.6px;
+  width: 200px;
+  padding-top: 12px;
 `;
