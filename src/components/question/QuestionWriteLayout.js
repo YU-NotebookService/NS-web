c4b319c49280ec249306587d2302fdd7de9d6a42
groundinsider <groundinsider24@gmail.com>
2024-10-04 00:23:25 +0900

    feat: 1:1문의 레이아웃 구현

--- src/components/question/QuestionWriteLayout.js
+++ src/components/question/QuestionWriteLayout.js
@@ -1,11 +1,10 @@
-import Input from 'components/common/Input';
-import Button from 'components/common/Button';
+import Register from 'components/common/Register';
 import { styled } from "styled-components";
 
 function QuestionWriteLayout() {
   return (  
     <>
-      
+      <Register />
     </>
   );
 }
