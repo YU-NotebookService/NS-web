6ae69415ffe6b50fc03e6cea3fc8afb9756211d8
gugitgugit <rnwnsgur28@gmail.com>
2024-11-16 00:12:41 +0900

    fix: 불필요 import 삭제

--- src/api/context/AuthProvider.js
+++ src/api/context/AuthProvider.js
@@ -1,4 +1,4 @@
-import React, { Children, createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const AuthContext = createContext();
 
