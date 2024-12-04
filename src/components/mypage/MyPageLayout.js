ab55955160316f28732a355511cebc9bac65e2b8
koo <rnwnsgur28@gmail.com>
2024-12-04 13:40:22 +0900

    feat: 마이페이지 대여 정보 조회 구현

--- src/components/mypage/MyPageLayout.js
+++ src/components/mypage/MyPageLayout.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { MyPageWrapper } from 'styles/MyPage-styled';
 import UserInfo from 'components/mypage/UserInfo';
 import NotebookRentalInfo from 'components/mypage/NotebookRentalInfo';
+import getMyRental from 'api/mypage/getMyRental';
 
 const MyPageLayout = () => {
+  const [myRental, setMyRental] = useState();
+
+  const fetchMyRental = useCallback(async () => {
+    try {
+      const response = await getMyRental();
+      setMyRental(response);
+    } catch (error) {
+      console.error(
+        '나의 대여 정보를 불러오는 데 실패하였습니다:',
+        error.message,
+      );
+    }
+  }, []); // 빈 배열로 인해 메모이제이션됨
+
+  useEffect(() => {
+    fetchMyRental();
+  }, [fetchMyRental]); // fetchMyRental을 의존성 배열에 추가
+
   return (
     <MyPageWrapper>
       <UserInfo name="홍길동" sid="22011475" phoneNum="010-9147-5631" />
-      <NotebookRentalInfo
-        notebookName="LG 13세대 울트라북 LG ULTRA 15U50R (32G/512G)"
-        rentalDate="2024-09-03 ~ 2024-10-10"
-      />
+      <NotebookRentalInfo myRentalInfo={myRental} />
     </MyPageWrapper>
   );
 };
--- src/components/mypage/NotebookRentalInfo.js
+++ src/components/mypage/NotebookRentalInfo.js
@@ -10,24 +10,38 @@ import {
   MyPageNotebookInfo,
 } from 'styles/MyPage-styled';
 
-const NotebookRentalInfo = ({ notebookName, rentalDate }) => {
-  const RentalInfo = [
-    { label: '모델명', text: notebookName },
-    { label: '대여 기간', text: rentalDate },
-  ];
+const NotebookRentalInfo = ({ myRentalInfo }) => {
+  const rentalInfo =
+    myRentalInfo && myRentalInfo.length > 0
+      ? [
+          { label: '노트북 ID', text: myRentalInfo.notbookId },
+          {
+            label: '대여 기간',
+            text: `${myRentalInfo.startDate} ~ ${myRentalInfo.endDate}`,
+          },
+        ]
+      : null;
+
   return (
     <MyPageNotebookInfo>
       <InfoTitle>대여 중인 노트북 정보</InfoTitle>
+
       <InfoContent>
-        {RentalInfo.map((el, index) => (
-          <InfoLine>
-            <InfoLabel>{el.label}</InfoLabel>
-            <InfoText>{el.text}</InfoText>
-          </InfoLine>
-        ))}
-        <ExtendBtnWrapper>
-          <ExtendBtn>연장 신청</ExtendBtn>
-        </ExtendBtnWrapper>
+        {rentalInfo ? (
+          <>
+            {rentalInfo.map((el, index) => (
+              <InfoLine key={index}>
+                <InfoLabel>{el.label}</InfoLabel>
+                <InfoText>{el.text}</InfoText>
+              </InfoLine>
+            ))}
+            <ExtendBtnWrapper>
+              <ExtendBtn>연장 신청</ExtendBtn>
+            </ExtendBtnWrapper>
+          </>
+        ) : (
+          <>대여중인 노트북이 없습니다.</>
+        )}
       </InfoContent>
     </MyPageNotebookInfo>
   );
