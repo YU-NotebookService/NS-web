d218a9265e8f3d49b30438ed65e0c86d1fc1f6d4
groundinsider <groundinsider24@gmail.com>
2024-12-06 01:36:27 +0900

    feat: 회원가입 입력 방식 수정

--- src/components/login/RegisterLayout.js
+++ src/components/login/RegisterLayout.js
@@ -120,49 +120,41 @@ function Register() {
             <NumInput
               value={input.NumText1}
               onChange={(e) => {
-                onChangeInput(e); // 기존 onChange 로직 호출
+                onChangeInput(e);
                 if (e.target.value.length === 3) {
-                  // 입력이 완료되면 다음 입력란으로 포커스 이동
                   document.getElementsByName('NumText2')[0].focus();
                 }
               }}
               name="NumText1"
               isError={!!errors.num}
-              maxLength={3} // 3자리까지만 입력
+              maxLength={3}
               style={{
                 left: '120px',
                 width: '33px',
               }}
             />
-
             <NumText>-</NumText>
-
-            {/* 두 번째 입력란 */}
             <NumInput
               value={input.NumText2}
               onChange={(e) => {
-                onChangeInput(e); // 기존 onChange 로직 호출
+                onChangeInput(e);
                 if (e.target.value.length === 4) {
-                  // 입력이 완료되면 다음 입력란으로 포커스 이동
                   document.getElementsByName('NumText3')[0].focus();
                 }
               }}
               name="NumText2"
               isError={!!errors.num}
-              maxLength={4} // 4자리까지만 입력
-              disabled={!input.NumText1} // 첫 번째 입력이 완료되지 않으면 비활성화
+              maxLength={4}
+              disabled={!input.NumText1}
             />
-
             <NumText>-</NumText>
-
-            {/* 세 번째 입력란 */}
             <NumInput
               value={input.NumText3}
-              onChange={onChangeInput} // 기존 onChange 로직
+              onChange={onChangeInput}
               name="NumText3"
               isError={!!errors.num}
-              maxLength={4} // 4자리까지만 입력
-              disabled={!input.NumText2} // 두 번째 입력이 완료되지 않으면 비활성화
+              maxLength={4}
+              disabled={!input.NumText2}
             />
           </FormItem>
           {errors.num && <ErrorMessage>{errors.num.message}</ErrorMessage>}
